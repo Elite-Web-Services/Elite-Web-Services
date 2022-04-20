937dd5a7413926a5c00b641ea55bdd1b947a9d0f
k-hongkham <k.hongkham@gmail.com>
2022-04-20 15:28:39 -0400

    sidebar merging.

--- src/components/SIDEBAR/Sidebar.jsx
+++ src/components/SIDEBAR/Sidebar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
+import Admin from "./Admin";
+import Cart from "./Cart";
+import Profile from "./Profile";
 
 const Sidebar = () => {
   const { user } = useAuth();
+  const [toggleAdminRender, setToggleAdminRender] = useState(false);
+  const [toggleCartRender, setToggleCartRender] = useState(false);
+  const [toggleProfileRender, setToggleProfileRender] = useState(false);
 
   return (
     <div id="sidebar_container">
       <div className=" flex-shrink-0 p-3 bg-white" style={{ width: "280px" }}>
         <ul className="list-unstyled ps-0">
+          {/* --------------------------------------------MY PROFILE */}
+
           <li className="mb-1">
             <button
               className="btn btn-toggle align-items-center rounded collapsed"
@@ -37,6 +45,34 @@ const Sidebar = () => {
               </ul>
             </div>
           </li>
+          {/* ----------------------------------------ADMIN */}
+          {user.isAdmin ? (
+            <li className="mb-1">
+              <button
+                className="btn btn-toggle align-items-center rounded collapsed"
+                data-bs-toggle="collapse"
+                data-bs-target="#cart-sidebar-collapse"
+                aria-expanded="false"
+              >
+                ADMIN Controls
+              </button>
+              <div className="collapse" id="cart-sidebar-collapse">
+                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Manage Users
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="link-dark rounded">
+                      Manage Products
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          ) : null}
+          {/* ----------------------------------------CART */}
           <li className="mb-1">
             <button
               className="btn btn-toggle align-items-center rounded collapsed"
@@ -62,9 +98,12 @@ const Sidebar = () => {
             </div>
           </li>
         </ul>
-        {/* terery admin button */}
 
-        <div className="side-bar-render-box">render container</div>
+        <div className="side-bar-render-box">
+          {/* {user.isAdmin && toggleAdminRender ? <Admin /> : null}
+          {toggleCartRender ? <Cart /> : null}
+          {toggleProfileRender ? <Profile /> : null} */}
+        </div>
       </div>
     </div>
   );
