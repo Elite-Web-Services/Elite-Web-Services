96dd0a823b31028c4f33075923b2386d42ba2644
danielforkner <daniel.forkner@gmail.com>
2022-04-28 09:50:07 -0400

    changed app-container to <main>

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
-import { getAPIHealth } from "../axios-services";
-import "../style/App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import AuthProvider from "./context/AuthContext";
-import Main from "./MAIN/Main";
-import Navbar from "./NAV/Navbar";
+import { getAPIHealth } from '../axios-services';
+import '../style/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './context/AuthContext';
+import Main from './MAIN/Main';
+import Navbar from './NAV/Navbar';
 
-import Sidebar from "./SIDEBAR/Sidebar"; //NEW
-import { BrowserRouter as Router } from "react-router-dom";
-import CartProvider from "./context/CartContext";
-import ProductProvider from "./context/ProductContext";
-import ContactProvider from "./context/ContactContext";
+import Sidebar from './SIDEBAR/Sidebar'; //NEW
+import { BrowserRouter as Router } from 'react-router-dom';
+import CartProvider from './context/CartContext';
+import ProductProvider from './context/ProductContext';
+import ContactProvider from './context/ContactContext';
 
 const App = () => {
-  const [APIHealth, setAPIHealth] = useState("");
+  const [APIHealth, setAPIHealth] = useState('');
 
   useEffect(() => {
     // follow this pattern inside your useEffect calls:
@@ -24,7 +24,7 @@ const App = () => {
     // invoke the adapter, await the response, and set the data
     const getAPIStatus = async () => {
       const { healthy } = await getAPIHealth();
-      setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
+      setAPIHealth(healthy ? 'api is up! :D' : 'api is down :/');
     };
 
     // second, after you've defined your getter above
@@ -38,8 +38,8 @@ const App = () => {
         <CartProvider>
           <ProductProvider>
             <ContactProvider>
-              <div className="app-container">
-                <h1 className="webTitle">{"ELITE . WEB . SERVICES"}</h1>
+              <main>
+                <h1 className="webTitle">{'ELITE . WEB . SERVICES'}</h1>
                 <Navbar />
                 {/* <p>API Status: {APIHealth}</p> */}
                 <div className="content d-flex justify-content-between">
@@ -47,7 +47,7 @@ const App = () => {
 
                   <Sidebar />
                 </div>
-              </div>
+              </main>
             </ContactProvider>
           </ProductProvider>
         </CartProvider>
