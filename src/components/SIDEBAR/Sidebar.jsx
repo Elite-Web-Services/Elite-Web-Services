699d3f8fb377f5b33c63bb9dce720e7fa794b772
danielforkner <daniel.forkner@gmail.com>
2022-04-28 13:26:43 -0400

    merge fix

--- src/components/SIDEBAR/Sidebar.jsx
+++ src/components/SIDEBAR/Sidebar.jsx
@@ -1,13 +1,10 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import useAuth from '../hooks/useAuth';
 import useCart from '../hooks/useCart';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { decrementQuantity, incrementQuantity } from '../context/helpers';
 
 const Sidebar = () => {
-  const location = useLocation();
-  console.log(location);
-  const [isHome, setIsHome] = useState(false);
   const { user, token } = useAuth();
   const { cart, setCart } = useCart();
   const [toggleAdminRender, setToggleAdminRender] = useState(false);
@@ -33,9 +30,8 @@ const Sidebar = () => {
   };
 
   return (
-
     <div className="sideBarContainer">
-      <div className="d-flex flex-column p-3 bg-whit sideBarWidth">
+      <div className="d-flex flex-column p-3 bg-white">
         <ul className="list-unstyled ps-0">
           {/* --------------------------------------------MY PROFILE */}
 
@@ -53,20 +49,13 @@ const Sidebar = () => {
               <div className="collapse" id="admin-sidebar-collapse">
                 <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                   <li>
-                    <Link to="/users" style={{ textDecoration: "none" }}>
-                      Manage Users{" "}
+                    <Link to="/users" style={{ textDecoration: 'none' }}>
+                      Manage Users{' '}
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      to="/manageproducts"
-                      onClick={() => {
-                        setToggleCheckout(true);
-                      }}
-                      style={{ textDecoration: "none" }}
-                    >
-
-                      Manage Products{" "}
+                    <Link to="/" style={{ textDecoration: 'none' }}>
+                      Manage Products{' '}
                     </Link>
                   </li>
                 </ul>
@@ -88,13 +77,13 @@ const Sidebar = () => {
               <div className="collapse" id="profile-collapse">
                 <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                   <li>
-                    <Link to="/orderHistory" style={{ textDecoration: "none" }}>
-                      Order History{" "}
+                    <Link to="/orderHistory" style={{ textDecoration: 'none' }}>
+                      Order History{' '}
                     </Link>
                   </li>
                   <li>
-                    <Link to="/contactinfo" style={{ textDecoration: "none" }}>
-                      Contact Information{" "}
+                    <Link to="/contactinfo" style={{ textDecoration: 'none' }}>
+                      Contact Information{' '}
                     </Link>
                   </li>
                 </ul>
@@ -117,105 +106,25 @@ const Sidebar = () => {
             <div className="collapse" id="cart-sidebar-collapse">
               <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                 <li>
-                  <Link to="/checkout" style={{ textDecoration: "none" }}>
-                    Checkout{" "}
+                  <Link to="/checkout" style={{ textDecoration: 'none' }}>
+                    Checkout{' '}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/cart" style={{ textDecoration: "none" }}>
-                    Go to Cart{" "}
+                  <Link to="/cart" style={{ textDecoration: 'none' }}>
+                    Go to Cart{' '}
                   </Link>
                 </li>
-              ) : (
-                <h3>GUEST ACCESS</h3>
-              )}
-              {/* ----------------------------------------CART */}
-              <li className="mb-1">
-                <button
-                  className="btn btn-toggle align-items-center rounded collapsed"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#cart-sidebar-collapse"
-                  aria-expanded="false"
-                  onClick={handleCart}
-                >
-                  Cart
-                </button>
-                <div className="collapse" id="cart-sidebar-collapse">
-                  <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li>
-                      <Link
-                        to="/checkout"
-                        onClick={() => {
-                          setToggleCheckout(true);
-                        }}
-                        style={{ textDecoration: 'none' }}
-                      >
-                        Checkout{' '}
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/cart"
-                        onClick={() => {
-                          setToggleCart(true);
-                        }}
-                        style={{ textDecoration: 'none' }}
-                      >
-                        Go to Cart{' '}
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-            {/* -------------------------------------RENDER MENU */}
-            <div className="col-md-5 col-lg-4 order-md-last">
-              <h4 className="d-flex justify-content-between align-items-center mb-3">
-                <span className="text-primary">Your cart</span>
-                <span className="badge bg-primary rounded-pill">
-                  {cart.products ? cart.products.length : '0'}
-                </span>
-              </h4>
-              <ul className="list-group mb-3">
-                {cart.products
-                  ? cart.products.map((product, i) => {
-                      return (
-                        <li
-                          className="list-group-item d-flex justify-content-between lh-sm"
-                          key={`checkoutcartproduct:${i}`}
-                        >
-                          <div>
-                            <h6 className="my-0">{product.name}</h6>
-
-                            <p>
-                              <strong>Quantity: {product.quantity}</strong>
-                            </p>
-                            <button
-                              onClick={() => handleIncrementClick(product)}
-                            >
-                              +
-                            </button>
-                            <button
-                              onClick={() => handleDecrementClick(product)}
-                            >
-                              -
-                            </button>
-                          </div>
-                        </li>
-                      );
-                    })
-                  : null}
               </ul>
             </div>
-
           </li>
         </ul>
         {/* -------------------------------------RENDER MENU */}
-        <div className="list-group list-group-flush border-bottom scrollarea">
+        <div className="col-md-5 col-lg-4 order-md-last">
           <h4 className="d-flex justify-content-between align-items-center mb-3">
             <span className="text-primary">Your cart</span>
             <span className="badge bg-primary rounded-pill">
-              {cart.products ? cart.products.length : "0"}
+              {cart.products ? cart.products.length : '0'}
             </span>
           </h4>
           <ul className="list-group mb-3">
@@ -244,10 +153,9 @@ const Sidebar = () => {
                 })
               : null}
           </ul>
-
         </div>
-      )}
-
+      </div>
+    </div>
   );
 };
 
