e90d60bdfa64edc8c919421d053df233d7b749d1
danielforkner <daniel.forkner@gmail.com>
2022-04-29 14:48:55 -0400

    cart style

--- src/components/SIDEBAR/Cart.jsx
+++ src/components/SIDEBAR/Cart.jsx
@@ -40,41 +40,44 @@ const Cart = () => {
           >
             {cart.products.map((product, index) => {
               return (
-                <div className="col">
+                <div className="col cartCard">
                   <div
                     key={'cart' + index}
                     className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
                     style={{ backgroundImage: `url(${product.imgURL})` }}
                   >
-                    <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                      <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                        {product.name}
-                      </h2>
-                      <h4>Quantity: {product.quantity}</h4>
-                      <ul className="d-flex list-unstyled mt-auto">
-                        <li className="me-auto">
-                          <Link to={`/viewproduct=${product.id}`}>
-                            <button className="btn btn-primary">Details</button>
-                          </Link>
-                        </li>
-                        <li className="me-auto">
-                          {' '}
+                    <div className="transparentLayer">
+                      <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                        <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                          {product.name}
+                        </h2>
+                        <h4>Quantity: {product.quantity}</h4>
+                        <div className="d-flex">
                           <button onClick={() => handleIncrementClick(product)}>
                             +
                           </button>
                           <button onClick={() => handleDecrementClick(product)}>
                             -
                           </button>
-                        </li>
-                        <li className="d-flex align-items-center me-3">
-                          <button
-                            className="btn btn-danger"
-                            onClick={() => removeProduct(product)}
-                          >
-                            Remove
-                          </button>
-                        </li>
-                      </ul>
+                        </div>
+                        <ul className="d-flex list-unstyled mt-auto">
+                          <li className="me-auto">
+                            <Link to={`/viewproduct=${product.id}`}>
+                              <button className="btn btn-outline-success">
+                                Details
+                              </button>
+                            </Link>
+                          </li>
+                          <li className="d-flex align-items-center me-3">
+                            <button
+                              className="btn btn-danger"
+                              onClick={() => removeProduct(product)}
+                            >
+                              Remove
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
